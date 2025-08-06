d17c2e6c9155f0bd9243cfd02bf86da301629314
Sanjay Sivakumar <sanjaysiva5209@gmail.com>
2025-08-06 10:18:49 +0530

    Add files via upload

---
+++ Day12.js
@@ -0,0 +1,19 @@
+a=15
+var b=66;
+let c=77;
+const d=88;
+console.log(a);
+
+let s=10;
+let z=20.22;
+let h="Hello";
+let r="world";
+let e=true;
+let f=null;
+console.log(e);
+console.log(f);
+
+let k=[1,2,3,4,5];
+let person={name:"John",age:30,city:"New york"};
+
+console.log(person.name + " is " + person.age + " years old lives in");
